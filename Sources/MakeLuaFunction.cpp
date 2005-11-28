9c1b8ceede48cd3d0e8eb846c0c15b17242c0436
Leandro Motta Barros <leandro_motta_barros@yahoo.com.br>
2005-11-28 11:32:54 -0200

    Added the missing 'MakeLuaFunction.cpp' file.

---
+++ Sources/MakeLuaFunction.cpp
@@ -0,0 +1,38 @@
+/******************************************************************************\
+* MakeLuaFunction.hpp                                                          *
+* Macros (yes, macros) to create 'lua_CFunctions' from slightly more friendly  *
+* functions taking (and possibly returning) 'LuaValue's.                       *
+* Leandro Motta Barros                                                         *
+\******************************************************************************/
+
+#include "MakeLuaFunction.hpp"
+
+namespace Diluculum
+{
+   namespace Impl
+   {
+      // - MLF_IssueLuaErrorIf  ------------------------------------------------
+      void MLF_IssueLuaErrorIf (lua_State* ls, bool condition,
+                                const std::string& function,
+                                const std::string& message)
+      {
+         if (condition)
+         {
+            lua_pushstring (ls, ("When calling '" + function + "()': "
+                                 + message).c_str());
+            lua_error (ls);
+         }
+      }
+
+
+
+      // - MLF_ReadParameters -----------------------------------------------------
+      void MLF_ReadParameters (lua_State* ls, Diluculum::LuaValue params[], int n)
+      {
+         for (int i = 0; i < n; ++i)
+            params[i] = Diluculum::ToLuaValue (ls, i+1);
+      }
+
+   } // namespace Impl
+
+} // namespace Diluculum
