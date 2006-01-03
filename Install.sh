5f01101ac92788072788883ab31d0f611f29cf02
Leandro Motta Barros <leandro_motta_barros@yahoo.com.br>
2006-01-03 14:35:12 -0200

    The 'Install.sh' script now works in rootless GoboLinux, too. (but this new version was not yet tested in a "real" GoboLinux machine)

--- Install.sh
+++ Install.sh
@@ -1,20 +1,37 @@
 #!/bin/sh
 
-if [ $# != 1 ]; then
-    echo "Usage: Install <version>"
-    exit 0
+# If this script has any chance of working, it is under GoboLinux.
+if [ ! $goboPrograms ]; then
+    echo "Sorry, this ad hoc installation script will work only under GoboLinux"
+    echo "(or, at least, Rootless GoboLinux). And, frankly, even under"
+    echo "GoboLinux things are not guaranteed..."
+    exit 1
 fi
 
+# Check command-line parameters
+if [ $# != 1 ]; then
+    echo "Usage: Install <version>"
+    exit 1
+fi
+
+# If we are running in a "real" (not rootless) GoboLinux, we'll have to use
+# 'sudo' to perform some instalation steps.
+if [ ! $ROOTLESS_GOBOLINUX ]; then
+    SUDO=sudo
+fi
+
+# If necessary, recompile the library (the optimized version)
 scons mode=opt
-sudo mkdir "/Programs/Diluculum"
-sudo mkdir "/Programs/Diluculum/$1"
-sudo mkdir "/Programs/Diluculum/$1/docs"
 
-sudo cp -r lib include README.txt HISTORY.txt "/Programs/Diluculum/$1"
-sudo rm "/Programs/Diluculum/$1/lib/.sconsign"
-
+# Remake the documentation
 doxygen
 
-sudo cp -r Documentation/html/* "/Programs/Diluculum/$1/docs"
-
+# Perform the actual instalation
+$SUDO mkdir -p "${goboPrograms}/Diluculum/$1/docs"
+$SUDO cp -r lib include README.txt HISTORY.txt "${goboPrograms}/Diluculum/$1"
+$SUDO rm "${goboPrograms}/Diluculum/$1/lib/.sconsign"
+$SUDO cp -r Documentation/html/* "${goboPrograms}/Diluculum/$1/docs"
 SymlinkProgram Diluculum "$1"
+
+# Done!
+exit 0
