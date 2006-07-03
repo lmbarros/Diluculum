5c3bbdf168dfb40c1c57db0dbf82fe8a42d58430
Leandro Motta Barros <lmb@stackedboxes.org>
2006-07-03 00:10:28 -0300

    Added the namespace to some occurrences of 'LuaState' in the documention of macros in 'LuaWrappers.hpp', so that Doxygen creates the proper hyperlinks.

--- include/Diluculum/LuaWrappers.hpp
+++ include/Diluculum/LuaWrappers.hpp
@@ -296,11 +296,11 @@ void Diluculum_Register_Class__ ## CLASS (Diluculum::LuaState& ls)            \
 
 
 
-/** Registers a class in a given \c LuaState. The class must have been
- *  previously exported by calls to \c DILUCULUM_BEGIN_CLASS,
+/** Registers a class in a given \c Diluculum::LuaState. The class must have
+ *  been previously exported by calls to \c DILUCULUM_BEGIN_CLASS,
  *  \c DILUCULUM_END_CLASS() and probably \c DILUCULUM_CLASS_METHOD().
- *  @param LUA_STATE The \c LuaState in which the class will be available after
- *         this call.
+ *  @param LUA_STATE The \c Diluculum::LuaState in which the class will be
+ *         available after this call.
  *  @param CLASS The class being registered.
  */
 #define DILUCULUM_REGISTER_CLASS(LUA_STATE, CLASS)  \
