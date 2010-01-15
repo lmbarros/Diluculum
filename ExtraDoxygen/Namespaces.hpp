3cb15cdc714a5ec91290e16ab3a1be737525af4d
Leandro Motta Barros <lmb@stackedboxes.org>
2010-01-15 20:34:26 -0200

    Updated the copyright messages years (it's 2010). Ignore-this: aedafa858b4f40ee8626b92edc81e3cf

--- ExtraDoxygen/Namespaces.hpp
+++ ExtraDoxygen/Namespaces.hpp
@@ -3,7 +3,7 @@
 * Additional Doxygen documentation for Diluculum: namespaces.                  *
 *                                                                              *
 *                                                                              *
-* Copyright (C) 2005-2009 by Leandro Motta Barros.                             *
+* Copyright (C) 2005-2010 by Leandro Motta Barros.                             *
 *                                                                              *
 * Permission is hereby granted, free of charge, to any person obtaining a copy *
 * of this software and associated documentation files (the "Software"), to     *
